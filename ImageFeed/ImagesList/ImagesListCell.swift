ad42bb170eb54e02167ef8f0850b5c22bb95752d
m.bastrykin <ump45x3@gmail.com>
2023-10-30 00:32:50 +0300

    дз по 8 спринту

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Миша Бастрыкин on 29.10.2023.
+//
+
+import Foundation
+import UIKit
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+
+
+}
